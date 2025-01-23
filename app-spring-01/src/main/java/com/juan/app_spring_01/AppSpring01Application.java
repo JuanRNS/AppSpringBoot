45ffdb64ba60903a63313328276aa8d3990914c4
Juan <juanramalho90@gmail.com>
2025-01-23 16:33:50 -0300

    primerio commit

---
+++ app-spring-01/src/main/java/com/juan/app_spring_01/AppSpring01Application.java
@@ -0,0 +1,13 @@
+package com.juan.app_spring_01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppSpring01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppSpring01Application.class, args);
+	}
+
+}
