4599bddb523fd06b6671ea65bbae9fa95b9f9864
Juan <juanramalho90@gmail.com>
2025-01-27 00:49:03 -0300

    -

--- app-spring-01/src/main/java/com/juan/app_spring_01/resource/PostResource.java
+++ app-spring-01/src/main/java/com/juan/app_spring_01/resource/PostResource.java
@@ -2,17 +2,11 @@ package com.juan.app_spring_01.resource;
 
 
 import com.juan.app_spring_01.domain.Post;
-import com.juan.app_spring_01.domain.User;
-import com.juan.app_spring_01.dto.UserDTO;
 import com.juan.app_spring_01.service.PostService;
-import com.juan.app_spring_01.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
-import java.net.URI;
-import java.util.List;
 
 
 @RestController
--- app-spring-01/src/main/java/com/juan/app_spring_01/service/PostService.java
+++ app-spring-01/src/main/java/com/juan/app_spring_01/service/PostService.java
@@ -1,15 +1,11 @@
 package com.juan.app_spring_01.service;
 
 import com.juan.app_spring_01.domain.Post;
-import com.juan.app_spring_01.domain.User;
-import com.juan.app_spring_01.dto.UserDTO;
 import com.juan.app_spring_01.repository.PostRepository;
-import com.juan.app_spring_01.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
-import java.util.Optional;
+
 
 @Service
 public class PostService {
